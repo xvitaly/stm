54fd341e75d570bc6b01fddd4a501faa9ab13833
V1TSK <vitaly@easycoding.org>
2014-02-07 19:16:59 +0600

    Uploaded project files.

---
+++ stm/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace stm
+{
+    class Program
+    {
+        static void ConfigureConsole(string Title, ConsoleColor Color)
+        {
+            Console.Title = Title;
+            Console.ForegroundColor = Color;
+        }
+
+        static void ShowSplash(string FileName)
+        {
+            using (Stream Strm = Assembly.GetExecutingAssembly().GetManifestResourceStream(FileName))
+            {
+                using (StreamReader Reader = new StreamReader(Strm))
+                {
+                    Console.WriteLine(Reader.ReadToEnd());
+                }
+            }
+        }
+
+        static string GeneratePOSTRequest(string AppID, string APIKey)
+        {
+            return String.Format("appid={0}&key={1}", AppID, APIKey);
+        }
+
+        static string SendAPIReq()
+        {
+            byte[] ByteReqC;
+            string Result = "";
+
+            HttpWebRequest WrQ = (HttpWebRequest)WebRequest.Create(Properties.Resources.AddURI);
+
+            WrQ.UserAgent = Properties.Resources.UserAgent;
+            WrQ.Method = "POST";
+            WrQ.Timeout = 250000;
+
+            ByteReqC = Encoding.UTF8.GetBytes(GeneratePOSTRequest("440", Properties.Settings.Default.APIKey));
+
+            WrQ.ContentType = "application/x-www-form-urlencoded";
+            WrQ.ContentLength = ByteReqC.Length;
+
+            using (Stream HTTPStreamRq = WrQ.GetRequestStream())
+            {
+                HTTPStreamRq.Write(ByteReqC, 0, ByteReqC.Length);
+                HTTPStreamRq.Close();
+            }
+
+            HttpWebResponse HTTPWResp = (HttpWebResponse)WrQ.GetResponse();
+
+            if (HTTPWResp.StatusCode == HttpStatusCode.OK)
+            {
+                using (Stream RespStream = HTTPWResp.GetResponseStream())
+                {
+                    using (StreamReader StrRead = new StreamReader(RespStream))
+                    {
+                        Result = StrRead.ReadToEnd();
+                    }
+                }
+            }
+            
+            return Result;
+        }
+
+        static string FetchStringHTTP()
+        {
+            string DnlStr = "";
+            using (WebClient Downloader = new WebClient())
+            {
+                Downloader.Headers.Add("User-Agent", Properties.Resources.UserAgent);
+                DnlStr = Downloader.DownloadString(String.Format(Properties.Resources.FetchURI, Properties.Settings.Default.APIKey));
+            }
+            return DnlStr;
+        }
+
+        static void GenerateSrvKey()
+        {
+            Console.Write(Properties.Resources.MsgGn);
+            try
+            {
+                XmlDocument XMLD = new XmlDocument();
+                XMLD.LoadXml(SendAPIReq());
+                Console.WriteLine(" Done.{0}", Environment.NewLine);
+                XmlNodeList XMLNList = XMLD.GetElementsByTagName("response");
+                for (int i = 0; i < XMLNList.Count; i++)
+                {
+                    Console.WriteLine(Properties.Resources.AddRes, XMLD.GetElementsByTagName("steamid")[i].InnerText, Environment.NewLine, XMLD.GetElementsByTagName("login_token")[i].InnerText);
+                }
+            }
+            catch (Exception Ex) { Console.WriteLine(String.Format(Properties.Resources.ExcptMsg, Ex.Message)); }
+        }
+
+        static void ListSrvKeys()
+        {
+            Console.Write(Properties.Resources.MsgFt);
+            try
+            {
+                XmlDocument XMLD = new XmlDocument();
+                XMLD.LoadXml(FetchStringHTTP());
+                Console.WriteLine(" Done.{0}", Environment.NewLine);
+                XmlNodeList XMLNList = XMLD.GetElementsByTagName("message");
+                for (int i = 0; i < XMLNList.Count; i++)
+                {
+                    Console.WriteLine(Properties.Resources.AddRes, XMLD.GetElementsByTagName("steamid")[i].InnerText, Environment.NewLine, XMLD.GetElementsByTagName("login_token")[i].InnerText);
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception Ex) { Console.WriteLine(String.Format(Properties.Resources.ExcptMsg, Ex.Message)); }
+        }
+        
+        static void Main(string[] Args)
+        {
+            ConfigureConsole(Properties.Resources.AppName, ConsoleColor.Green);
+            try { ShowSplash(Properties.Resources.WelcomeFileName); } catch (Exception Ex) { Console.WriteLine(String.Format(Properties.Resources.ExcptMsg, Ex.Message)); }
+
+            if (!(String.IsNullOrWhiteSpace(Properties.Settings.Default.APIKey)))
+            {
+                if (Args.Count() > 0)
+                {
+                    switch (Args[0])
+                    {
+                        case "generate": GenerateSrvKey();
+                            break;
+                        case "list": ListSrvKeys();
+                            break;
+                        default: Console.WriteLine(Properties.Resources.UnknownOpt);
+                            break;
+                    }
+                }
+                else { Console.WriteLine(Properties.Resources.WlxMsg); }
+            }
+            else { Console.WriteLine(Properties.Resources.NoAPIKey); }
+
+            #if DEBUG
+            Console.ReadKey();
+            #endif
+        }
+    }
+}
---
+++ stm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Simple Servers Manager")]
+[assembly: AssemblyDescription("Simple Servers Manager by EasyCoding Team")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EasyCoding Team")]
+[assembly: AssemblyProduct("Simple Servers Manager")]
+[assembly: AssemblyCopyright("(c) 2005-2014 EasyCoding Team. All rights reserved.")]
+[assembly: AssemblyTrademark("EasyCoding, EasyCoding Software Group and EasyCoding Team are trademarks of EasyCoding Team.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25deec73-bc07-4614-a71a-0bcc730b91fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.7")]
+[assembly: AssemblyFileVersion("0.1.0.7")]
